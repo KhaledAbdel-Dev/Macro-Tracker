3d01cf5052ce17fe7ff6fbd105f569b2cc666643
khaled <khaledabdel.dev@gmail.com>
2022-11-06 22:38:27 -0500

    Personal FullStack #2

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://khaled:Jamilo78@cluster0.77z2sbo.mongodb.net/Timesheet?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
